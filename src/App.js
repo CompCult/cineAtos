a0954f18384062090d3f2fe26b8281b15a77926c
AdielAndrade <adiel.rocha@ccc.ufcg.edu.br>
2019-05-24 12:11:45 -0300

    it's a fun commit to start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Podem começar a trabalhar...
         </p>
+        <h1>My puppets</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cineatos.herokuapp.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Site base do administrador 
         </a>
+        <h3>Agora vão trabalhar <b>!!!!!</b></h3>
       </header>
     </div>
   );
