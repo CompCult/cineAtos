af6502fe3627b87b74471a3f33c4efb781734fd3
lucasvduarte <lucvduarte@gmail.com>
2020-03-07 19:34:15 -0300

    troca de redux form para formik em login

--- src/screen/login/Login.js
+++ src/screen/login/Login.js
@@ -1,35 +1,17 @@
 import React, { useState } from "react";
 import LoginApi from "./LoginApi.js";
-import { Field, reduxForm } from "redux-form";
-import Button from "@material-ui/core/Button";
 import logo from "../../../src/images/images.png";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { makeStyles } from "@material-ui/core/styles";
 import { login, id, gestor, permissaoProfessor } from "../../services/Auth";
 import { useHistory } from "react-router-dom";
-import { RenderTextField } from "../../components/form/Form";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import IconButton from "@material-ui/core/IconButton";
 import Card from '@material-ui/core/Card';
 import { toast } from "react-toastify";
-
-const validate = values => {
-  const errors = {};
-  const requiredFields = ["email", "password"];
-  requiredFields.forEach(field => {
-    if (!values[field]) {
-      errors[field] = "Campo não pode ser vazio";
-    }
-  });
-
-  return errors;
-};
+import Form from './Form';
 
 const useStyles = makeStyles(theme => ({
   root: {
     width: '50%',
-    marginTop: '2%',
+    marginTop: '5%',
     marginBottom: '2%',
     marginLeft: 'auto',
     marginRight: 'auto',
@@ -49,25 +31,7 @@ const useStyles = makeStyles(theme => ({
 function Login() {
   const classes = useStyles();
   let history = useHistory();
-  const [showPassword, setShowPassword] = useState(false);
-  const [values, setValues] = useState({
-    email: "",
-    password: ""
-  });
-
-  const [request, setRequest] = useState(false);
-
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleMouseDownPassword = event => {
-    event.preventDefault();
-  };
+  const [values] = useState({ email: "", password: "" });
 
   const toSetUpAuth = (data) => {
     if (data.type === 'gestor') {
@@ -79,15 +43,13 @@ function Login() {
     id(data._id);
   }
 
-  const postLoginUser = async event => {
-    event.preventDefault();
-    setRequest(true);
+  const handleSubmit = async event => {
     await LoginApi.postLoginApi(values).then(res => {
       toSetUpAuth(res.data)
       history.push("/")
     }).catch(error => {
       toast.error("Email ou senha incorretas");
-    }).finally(() => setRequest(false))
+    })
   };
 
   return (
@@ -95,47 +57,11 @@ function Login() {
 
       <Card style={{ textAlign: 'center' }}>
         <img src={logo} className={classes.logo} alt="logo" />
+        <Form className={classes.marginButton} handleSubmit={handleSubmit} initialValues={values} />
 
-        <form onSubmit={postLoginUser} className='form'>
-          <Field onChange={handleChange("email")}
-            name="email"
-            component={RenderTextField}
-            label="Name ou Email" />
-          <div>
-            <Field onChange={handleChange("password")}
-              name="password"
-              label="Password"
-              component={RenderTextField}
-              type={showPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      edge="end"
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword} >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-          </div>
-          <Button type="submit"
-            variant="contained"
-            size="large"
-            color="primary"
-            className={classes.marginButton} disabled={!request ? false : true}>
-            {!request ? 'Login' : 'Carregando...'}
-          </Button>
-        </form>
       </Card>
     </div>
   );
 }
 
-export default reduxForm({
-  form: "MaterialUiFormLogin", // a unique identifier for this form
-  validate
-})(Login);
+export default Login;
