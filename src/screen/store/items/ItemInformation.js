153e77757839ee987fb0b3326e623a7b45e89e94
lvduarte <lucvduarte@gmail.com>
2020-08-01 22:53:57 -0300

    adicao de audio e video em info de loja de alunos

--- src/screen/store/items/ItemInformation.js
+++ src/screen/store/items/ItemInformation.js
@@ -8,7 +8,11 @@ const informationSyles = makeStyles(theme => ({
     width: 150,
     height: 180
   },
-
+  video: {
+    width: '100%',
+    maxWidth: 500,
+    margin: '1%'
+  },
   informacoes: {
     display: "flex",
     flexDirection: "column",
@@ -18,6 +22,28 @@ const informationSyles = makeStyles(theme => ({
 
 const ItemInformation = ({ item }) => {
   const styles = informationSyles();
+
+  const itemsPupils = (data) => {
+    console.log(data)
+    return <div>
+      {data.has_text && <p>{data.text}</p>}
+      {
+        data.has_video && (
+          <video src={data.video} controls className={styles.video}>
+            Seu navegador não suporta o elemento <code>video</code>.
+          </video>
+        )
+      }
+      {
+        data.has_audio && (
+          <audio src={data.audio} controls loop>
+            Navegador não suporta
+          </audio>
+        )
+      }
+    </div>
+  }
+
   return (
     <div className={styles.informacoes}>
       <img
@@ -26,8 +52,9 @@ const ItemInformation = ({ item }) => {
         alt="Item"
       />
       <h1>
-        {item.title} - {item.description}
+        {item.title} {(item.isCreatedByMission ? '' : " - " + item.description)}
       </h1>
+      {item.isCreatedByMission ? itemsPupils(item) : <></>}
       <h2>Quantidade: {item.quantity}</h2>
       <h2>Valor: {item.value}</h2>
       <h2>
